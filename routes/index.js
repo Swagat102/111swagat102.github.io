600141159cc46f7ec46e43f8d1e47ca9525cb85b
Swagat102 <97602607+Swagat102@users.noreply.github.com>
2022-08-16 23:45:16 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var nodemailer = require('nodemailer');
 var router = express.Router();
 
 /* GET home page. */
-router.get('', function(req, res) {
-  res.sendFile('home.html', { root: '.' });
+router.get('/', function(req, res) {
+  res.sendFile('index.html', { root: '.' });
 });
 
 /*router.get('/home', function(req, res, next) {
