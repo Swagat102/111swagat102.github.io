4211803542f49c898a811e1f3f39862efabe5159
Swagat102 <97602607+Swagat102@users.noreply.github.com>
2022-08-16 22:27:16 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res) {
 });
 
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { root: __dirname });
 });
 
 router.get('/about', function(req, res, next) {
